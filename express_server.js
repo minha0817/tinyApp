453e3faa213f606109048c277766f66c35f2a6b1
minha0817 <minhakk24@gmail.com>
2022-10-03 19:03:36 -0700

    Add a GET Route to Show the Form and Getting Ready for POST Requests

--- express_server.js
+++ express_server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
 
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
@@ -29,10 +30,19 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:id", (req, res) => {
   const templateVars = {
     id: req.params.id,
     longURL: urlDatabase[req.params.id],
   };
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send("Ok");
 });
