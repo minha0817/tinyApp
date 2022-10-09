28dc17d398da2e24c6e8a9dcffaf340446fc550e
minha0817 <minhakk24@gmail.com>
2022-10-08 19:36:14 -0700

    created database file to keep databases on this file

---
+++ database.js
@@ -0,0 +1,29 @@
+const urlDatabase = {
+  b6UTxQ: {
+    longURL: "https://www.tsn.ca",
+    userID: "aJ48lW",
+  },
+  i3BoGr: {
+    longURL: "https://www.google.ca",
+    userID: "aJ48lW",
+  },
+  minha: {
+    longURL: "https://lighthouselabs.ca",
+    userID: "minha",
+  },
+};
+
+const users = {
+  minha: {
+    id: "minha",
+    email: "minha@mail.com",
+    password: "$2a$10$muBVIbxl3vTPVD.Gi4SudeNdps7W3JIkldddvH52lOlo8zr1M1Tze",
+  },
+  chris: {
+    id: "chris",
+    email: "chris@mail.com",
+    password: "$2a$10$muBVIbxl3vTPVD.Gi4SudeNdps7W3JIkldddvH52lOlo8zr1M1Tze",
+  },
+};
+
+module.exports = { urlDatabase, users };
